263e976ca60e591df194cfdbb1af88aff7d32b42
tommstark <posik.tomas@gmail.com>
2019-06-13 21:08:44 -0300

    add animation to buttons

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const PlayerCard = ({ color, symbol }) => {
+const PlayerCard = ({ color, symbol, win, round }) => {
   const style = {
     backgroundColor: color,
     backgroundImage: `url(../img/${symbol}.png)`,
     backgroundSize: 'cover',
-    border: `black solid 6px`
   }
-  return (<div style={style} className="player-card"></div>)
+  return (<div style={style} className={`player-card ${ round == 0 ? 'opening-animation' : ''} ${win == color ? 'winner' : '' }`}></div>)
 }
 
-const NameCount = ({user, count}) => {
+const NameCount = ({ user, count }) => {
   return (
-    <h2 style={{flexGrow:2}}>{user ? 'You : ' : "Cpu : "} {[count]} </h2>
+    <h2 style={{ flexGrow: 2 }}>{user ? 'You : ' : "Cpu : "} {[count]} </h2>
   )
 }
 const PlayerButton = ({ color, symbol, onselect, disabled }) => {
   const style = {
-    border: `black solid 5px`,
-    padding: 0,
-    height: '10em',
-    flex: 1,
     backgroundImage: `url(../img/${symbol}.png)`,
     backgroundSize: 'cover',
     backgroundColor: color,
-    margin: '10px'
   }
   return (
-    <button style={style} onClick={onselect} disabled={disabled} className='selectButton'></button>
+    <button className='select-btn' style={style} onClick={onselect} disabled={disabled} ></button>
   )
 }
 class App extends Component {
 
   constructor(props) {
+
     super(props)
     this.symbols = ['rock', 'paper', 'scissors']
     this.colors = ['#16B8F8', '#039739', '#F15CB6']
@@ -44,7 +39,8 @@ class App extends Component {
       winner: " ",
       round: 0,
       redWin: 0,
-      yellowWin: 0
+      yellowWin: 0,
+      win: null,
     }
     this.runGame = this.runGame.bind(this);
   }
@@ -58,14 +54,16 @@ class App extends Component {
       (playerRed === 'paper' && playerYellow === "rock") ||
       (playerRed === 'scissors' && playerYellow === "paper")) {
       this.setState((prevState, props) => ({
-        redWin: prevState.redWin + 1
+        redWin: prevState.redWin + 1,
+        win: "#FA5F5C"
       }))
       return (
         "red player Wins"
       )
     } else {
       this.setState((prevState, props) => ({
-        yellowWin: prevState.yellowWin + 1
+        yellowWin: prevState.yellowWin + 1,
+        win: "#F6C318"
       }))
       return "Yellow player Wins"
     }
@@ -75,7 +73,8 @@ class App extends Component {
     this.setState((prevState, props) => ({
       disabled: true,
       playerRed: symbol,
-      round: prevState.round + 1
+      round: prevState.round + 1,
+      win: null,
     }))
     let counter = 0;
     let myinterval = setInterval(() => {
@@ -107,10 +106,14 @@ class App extends Component {
             <PlayerCard
               color="#FA5F5C"
               symbol={this.state.playerRed}
+              win={this.state.win}
+              round={this.state.round}
             />
             <PlayerCard
               color="#F6C318"
               symbol={this.state.playerYellow}
+              win={this.state.win}
+              round={this.state.round}
             />
           </div>
           {/* <button className='randomButton' onClick={this.runGame} disabled={this.state.disabled}>
@@ -121,7 +124,7 @@ class App extends Component {
               user={true}
               count={this.state.redWin}
             />
-            <h2 style={{display:'inlineBlock'}}>[{this.state.round}]</h2>
+            <h2 style={{ display: 'inlineBlock' }}>[{this.state.round}]</h2>
             <NameCount
               user={false}
               count={this.state.yellowWin}
